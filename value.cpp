0b85f7ba476516cc9bef42463ba9885b8aea5c71
danielpadkins <danielpadk@gmail.com>
2023-12-10 00:06:27 -0500

    init mlp and start testing (WIP)

--- value.cpp
+++ value.cpp
@@ -74,6 +74,124 @@ public:
 
 };
 
+class Module {
+public:
+    virtual void zero_grad() {
+        for (auto& p : parameters()) {
+            p->grad = 0;
+        }
+    }
+
+    virtual std::vector<std::shared_ptr<Value>> parameters() {
+        return {};
+    }
+};
+
+class Neuron : public Module {
+public:
+    std::vector<std::shared_ptr<Value>> w;
+    std::shared_ptr<Value> b;
+    bool nonlin;
+
+    Neuron(int nin, bool nonlin=true) : nonlin(nonlin) {
+        for (int i = 0; i < nin; ++i) {
+            w.push_back(std::make_shared<Value>(1.0));
+        }
+        b = std::make_shared<Value>(0.0);
+    }
+
+    std::shared_ptr<Value> operator()(std::vector<std::shared_ptr<Value>> x) {
+        auto act = b;
+        for (int i = 0; i < x.size(); ++i) {
+            act = Value::add(act, Value::multiply(w[i], x[i]));
+        }
+        return nonlin ? act : act;
+    }
+
+    std::vector<std::shared_ptr<Value>> parameters() override {
+        std::vector<std::shared_ptr<Value>> out;
+        for (auto& wi : w) {
+            out.push_back(wi);
+        }
+        out.push_back(b);
+        return out;
+    }
+
+    std::string repr() {
+        return "Neuron";
+    }
+};
+
+class Layer : public Module {
+public:
+    std::vector<std::shared_ptr<Neuron>> neurons;
+
+    Layer(int nin, int nout, bool nonlin=true) {
+        for (int i = 0; i < nout; ++i) {
+            neurons.push_back(std::make_shared<Neuron>(nin, nonlin));
+        }
+    }
+
+    std::vector<std::shared_ptr<Value>> operator()(std::vector<std::shared_ptr<Value>> x) {
+        std::vector<std::shared_ptr<Value>> out;
+        for (auto& neuron : neurons) {
+            out.push_back((*neuron)(x));
+        }
+        return out;
+    }
+
+    std::vector<std::shared_ptr<Value>> parameters() override {
+        std::vector<std::shared_ptr<Value>> out;
+        for (auto& neuron : neurons) {
+            for (auto& p : neuron->parameters()) {
+                out.push_back(p);
+            }
+        }
+        return out;
+    }
+
+    std::string repr() {
+        return "Layer";
+    }
+};
+
+class MLP : public Module {
+public:
+    std::vector<std::shared_ptr<Layer>> layers;
+
+    MLP(int nin, std::vector<int> nouts) {
+        std::vector<int> sz;
+        sz.push_back(nin);
+        for (auto& nout : nouts) {
+            sz.push_back(nout);
+        }
+        for (int i = 0; i < nouts.size(); ++i) {
+            layers.push_back(std::make_shared<Layer>(sz[i], sz[i+1], i != nouts.size()-1));
+        }
+    }
+
+    std::vector<std::shared_ptr<Value>> operator()(std::vector<std::shared_ptr<Value>> x) {
+        for (auto& layer : layers) {
+            x = (*layer)(x);
+        }
+        return x;
+    }
+
+    std::vector<std::shared_ptr<Value>> parameters() override {
+        std::vector<std::shared_ptr<Value>> out;
+        for (auto& layer : layers) {
+            for (auto& p : layer->parameters()) {
+                out.push_back(p);
+            }
+        }
+        return out;
+    }
+
+    std::string repr() {
+        return "MLP";
+    }
+};
+
 // test grad calculation
 void test_grad() {
     auto a = std::make_shared<Value>(1.0);
@@ -92,11 +210,20 @@ void test_grad() {
     assert(c->grad == 4);
     assert(d->grad == 3);
 
-    std::cout << "grad test passed" << std::endl;
+    std::cout << "Passed: test_grad" << std::endl;
 }
+
+void test_mlp() {
+    auto mlp = MLP(2, {3, 1});
+    auto x = std::vector<std::shared_ptr<Value>>{std::make_shared<Value>(1.0), std::make_shared<Value>(2.0)};
+    auto y = mlp(x);
+    y[0]->backward(y[0]); 
+}
+
 
 // main func
 int main() {
     test_grad();
+    test_mlp();
     return 0;
 }
